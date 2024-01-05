852c2b1eb7970eb89028aaa806fb0ada1bc80757
Marcos Vinicius <marcospeixotogk@gmail.com>
2024-01-05 19:41:46 -0300

    refactor pagina configuracoes

--- lib/pages/configuracoes.dart
+++ lib/pages/configuracoes.dart
@@ -1,14 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:mobirural/constants/appconstants.dart';
 import 'package:mobirural/pages/teste.dart';
-
+import 'package:flutter/material.dart';
+import 'package:mobirural/constants/appconstants.dart';
+import 'package:mobirural/models/user_model.dart';
+import 'package:mobirural/pages/configuracoes.dart';
+import 'package:provider/provider.dart';
 class ConfiguracoesPage extends StatelessWidget {
   
 
   const ConfiguracoesPage({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
+    Widget acessibilidadeButton = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            side: const BorderSide(color: Colors.transparent),
+          ),
+        ),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const ConfiguracoesPage()));
+        },
+      child: const Text('Configurações de acessibilidade'))
+      );
+      
+    Widget reportarProblemaButton = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            side: const BorderSide(color: Colors.transparent),
+          ),
+        ),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const ConfiguracoesPage()));
+        },
+      child: const Text('Reportar Problema',),
+      )
+    );
+      
+    Widget logoutButton = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            side: const BorderSide(color: Colors.transparent),
+          ),
+        ),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const ConfiguracoesPage()));
+        },
+      child: const Text('Logout'),
+      )
+    );
+      
+    Widget sairButton = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            side: const BorderSide(color: Colors.transparent),
+          ),
+        ),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const ConfiguracoesPage()));
+        },
+      child: const Text('Sair'),
+      )
+      
+    );
     return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
       appBar: AppBar(
         title: const Text('Configurações'),
       ),
@@ -16,25 +105,10 @@ class ConfiguracoesPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [            
-            TextButton(
-              child: const Text('Configurações de acessibilidade'),
-              onPressed: (){
-              }),
-            TextButton(
-              child: const Text('Reportar Problema'),
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const TestePage()));
-              }),
-            TextButton(
-              child: const Text('Logout'),
-              onPressed: (){
-              }),
-            TextButton(
-              child: const Text('Sair'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
+            acessibilidadeButton,
+            reportarProblemaButton,
+            logoutButton,
+            sairButton
           ],
         ),
       ),
