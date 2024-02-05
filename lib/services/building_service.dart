e46fe772ea2b73145099481ba0a706b884d997a3
filipelion <14293424+Filipelion@users.noreply.github.com>
2024-02-05 15:41:07 -0300

    feat: Adicionando criar, atualizar e deletar prédios na service

--- lib/services/building_service.dart
+++ lib/services/building_service.dart
@@ -9,13 +9,15 @@ class BuildingService extends ChangeNotifier {
     List<Building> buildings = [];
 
     try {
-      QuerySnapshot querySnapshot = await _firestore.collection('buildings').get();
-      
+      QuerySnapshot querySnapshot =
+          await _firestore.collection('buildings').get();
+
       for (var doc in querySnapshot.docs) {
-        Building building = Building.fromMap(doc.data() as Map<String, dynamic>?);
+        Building building =
+            Building.fromMap(doc.data() as Map<String, dynamic>?);
         building.id = doc.id;
         buildings.add(building);
-            }
+      }
 
       return buildings;
     } catch (e) {
@@ -23,16 +25,62 @@ class BuildingService extends ChangeNotifier {
       return [];
     }
   }
+
   Future<List<Building>> getBuildingsByIds(List<String> buildingIds) async {
     try {
       List<Building> buildings = [];
       for (String id in buildingIds) {
-        DocumentSnapshot doc = await _firestore.collection('buildings').doc(id).get();
+        DocumentSnapshot doc =
+            await _firestore.collection('buildings').doc(id).get();
         if (doc.exists) {
           buildings.add(Building.fromMap(doc.data() as Map<String, dynamic>?));
         }
       }
       return buildings;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> createBuilding(Building building) async {
+    try {
+      await _firestore.collection('buildings').add({
+        'access_ramps': building.accessRamps,
+        'adapted_bathroom': building.adaptedBathroom,
+        'coordinates': building.coordinates,
+        'elevator': building.elevator,
+        'floor': building.floor,
+        'icon': building.icon,
+        'name': building.name,
+        'parking': building.parking,
+        'image': building.image,
+      });
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> updateBuilding(Building building) async {
+    try {
+      await _firestore.collection('buildings').doc(building.id).update({
+        'access_ramps': building.accessRamps,
+        'adapted_bathroom': building.adaptedBathroom,
+        'coordinates': building.coordinates,
+        'elevator': building.elevator,
+        'floor': building.floor,
+        'icon': building.icon,
+        'name': building.name,
+        'parking': building.parking,
+        'image': building.image,
+      });
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> deleteBuilding(String buildingId) async {
+    try {
+      await _firestore.collection('buildings').doc(buildingId).delete();
     } catch (e) {
       rethrow;
     }
